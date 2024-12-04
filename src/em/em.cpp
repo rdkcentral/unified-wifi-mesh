4d459fd3826f2fac0f4ae889059974ec7d86d7a6
Tucker Polomik <t.polomik@cablelabs.com>
2024-12-04 13:52:34 -0700

    em.cpp: forward provisioning msgs to em_provisioning::process_msg
    Signed-off-by: Tucker Polomik <t.polomik@cablelabs.com>

--- src/em/em.cpp
+++ src/em/em.cpp
@@ -212,6 +212,18 @@ void em_t::proto_process(unsigned char *data, unsigned int len)
             em_metrics_t::process_msg(data, len);
             break;
 
+        case em_msg_type_dpp_cce_ind:
+        case em_msg_type_proxied_encap_dpp:
+        case em_msg_type_direct_encap_dpp:
+        case em_msg_type_reconfig_trigger:
+        case em_msg_type_bss_config_req:
+        case em_msg_type_bss_config_rsp:
+        case em_msg_type_bss_config_res:
+        case em_msg_type_chirp_notif:
+        case em_msg_type_dpp_bootstrap_uri_notif:
+            em_provisioning_t::process_msg(data, len);
+            break;
+
         default:
             break;  
     }
