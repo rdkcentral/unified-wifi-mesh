22b6be00d375c9e0f200781126fbd7f1231d1a21
Benjamin Carlson <b.carlson@cablelabs.com>
2024-12-04 15:32:45 -0700

    Rebase fixup commit
    Signed-off-by: Benjamin Carlson <b.carlson@cablelabs.com>

--- src/em/crypto/em_crypto.cpp
+++ src/em/crypto/em_crypto.cpp
@@ -51,14 +51,13 @@
 #include "util.h"
 #include <iostream>
 #include <openssl/dh.h>
-#include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
 #include <fstream>
 #include <sstream>
 #include <string>
 
-#define OPENSSL_VERSION_NUMBER 0x10100000L
+//#define OPENSSL_VERSION_NUMBER 0x10100000L
 // Initialize the static member variables
 // From RFC 3526
 uint8_t em_crypto_t::g_dh1536_p[] =  {
@@ -216,7 +215,6 @@ bail:
     return -1;
 }
 
-
 uint8_t em_crypto_t::platform_hash(const EVP_MD * hashing_algo, uint8_t num_elem, uint8_t **addr, uint32_t *len, uint8_t *digest)
 {  
     EVP_MD_CTX   *ctx;
@@ -236,7 +234,7 @@ uint8_t em_crypto_t::platform_hash(const EVP_MD * hashing_algo, uint8_t num_elem
     EVP_MD_CTX_init(ctx);
 #endif
 
-    if (!EVP_DigestInit_ex(ctx, EVP_sha256(), NULL)) {
+    if (!EVP_DigestInit_ex(ctx, hashing_algo, NULL)) {
         res = 0;
     }
 
@@ -263,7 +261,7 @@ uint8_t em_crypto_t::platform_hash(const EVP_MD * hashing_algo, uint8_t num_elem
 
     return res;
 }
-uint8_t em_crypto_t::platform_hmac_SHA256(uint8_t *key, uint32_t keylen, uint8_t num_elem, uint8_t **addr,
+uint8_t em_crypto_t::platform_hmac_hash(const EVP_MD * hashing_algo, uint8_t *key, uint32_t keylen, uint8_t num_elem, uint8_t **addr,
         uint32_t *len, uint8_t *hmac)
 {
     //em_util_info_print(EM_CONF," %s:%d\n",__func__,__LINE__);
@@ -297,7 +295,7 @@ uint8_t em_crypto_t::platform_hmac_SHA256(uint8_t *key, uint32_t keylen, uint8_t
     if (pkey == NULL) {
         goto bail;
     }
-    if (EVP_DigestSignInit(ctx, NULL, EVP_sha256(), NULL, pkey) != 1) {
+    if (EVP_DigestSignInit(ctx, NULL, hashing_algo, NULL, pkey) != 1) {
         goto bail;
     }
 
@@ -309,7 +307,7 @@ uint8_t em_crypto_t::platform_hmac_SHA256(uint8_t *key, uint32_t keylen, uint8_t
         goto bail;
     }
 #else
-    if (HMAC_Init_ex(ctx, key, keylen, EVP_sha256(), NULL) != 1) {
+    if (HMAC_Init_ex(ctx, key, keylen, hashing_algo, NULL) != 1) {
         goto bail;
     }
 
@@ -410,7 +408,7 @@ uint8_t em_crypto_t:: wps_key_derivation_function(uint8_t *key, uint8_t *label_p
     }
     return 1; 
 }
-uint8_t em_crypto_t::platform_aes_encrypt(uint8_t *key, uint8_t *iv, uint8_t *plain, uint32_t plain_len, uint8_t *cipher, uint32_t *cipher_len)
+uint8_t em_crypto_t::platform_cipher_encrypt(const EVP_CIPHER *cipher_type, uint8_t *key, uint8_t *iv, uint8_t *plain, uint32_t plain_len, uint8_t *cipher, uint32_t *cipher_len)
 {
     if (cipher_type == NULL){
         return 0;
@@ -429,7 +427,7 @@ uint8_t em_crypto_t::platform_aes_encrypt(uint8_t *key, uint8_t *iv, uint8_t *pl
         return 0;
     }
 #endif
-    if (EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1) {
+    if (EVP_EncryptInit_ex2(ctx, cipher_type, key, iv, NULL) != 1) {
         return 0;
     }
 
@@ -456,7 +454,7 @@ uint8_t em_crypto_t::platform_aes_encrypt(uint8_t *key, uint8_t *iv, uint8_t *pl
 
     return 1;
 }
-uint8_t em_crypto_t::platform_aes_decrypt(uint8_t *key, uint8_t *iv, uint8_t *data, uint32_t data_len)
+uint8_t em_crypto_t::platform_cipher_decrypt(const EVP_CIPHER *cipher_type, uint8_t *key, uint8_t *iv, uint8_t *data, uint32_t data_len)
 {
     if (cipher_type == NULL){
         return 0;
@@ -476,7 +474,7 @@ uint8_t em_crypto_t::platform_aes_decrypt(uint8_t *key, uint8_t *iv, uint8_t *da
         return 0;
     }
 #endif
-    if (EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1) {
+    if (EVP_DecryptInit_ex2(ctx, cipher_type, key, iv, NULL)  != 1) {
         return 0;
     }
 
@@ -500,6 +498,7 @@ uint8_t em_crypto_t::platform_aes_decrypt(uint8_t *key, uint8_t *iv, uint8_t *da
 
     return 1;
 }
+
 
 EVP_PKEY* em_crypto_t::create_dh_pkey(BIGNUM *p, BIGNUM *g, BIGNUM *bn_priv, BIGNUM *bn_pub)
 {
@@ -689,4 +688,3 @@ uint8_t em_crypto_t::compute_secret_internal(BIGNUM *p, BIGNUM *g, BIGNUM *bn_pr
     return (*secret_len > 0) ? 1 : 0;
 }
 #endif
-
