f60de93b5730a39a033efacf1c280ad4a522a49b
Ben Carlson <b.carlson@cablelabs.com>
2025-03-24 14:42:00 -0700

    Fix warnings in EasyConnect code
    Signed-off-by: Ben Carlson <b.carlson@cablelabs.com>

--- src/em/prov/easyconnect/ec_util.cpp
+++ src/em/prov/easyconnect/ec_util.cpp
@@ -186,7 +186,6 @@ std::pair<uint8_t*, uint16_t> ec_util::unwrap_wrapped_attrib(ec_attribute_t *wra
 
     uint8_t* wrapped_ciphertext = wrapped_attrib->data + AES_BLOCK_SIZE;
     uint16_t wrapped_len = wrapped_attrib->length - AES_BLOCK_SIZE;
-    size_t pre_wrapped_attribs_size;
 
     uint8_t* unwrap_attribs = new uint8_t[wrapped_len]();
     int result = -1;
