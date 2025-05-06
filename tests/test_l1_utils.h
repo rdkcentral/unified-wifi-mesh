2893216ae82a6472f3994f4afcc530abc019547c
Radhakrishnan, Tarunikaa <Tarunikaa_Radhakrishnan@comcast.com>
2025-05-06 07:54:31 +0000

    Issue #347: Push L1 unit tests
    Signed-off-by: Radhakrishnan, Tarunikaa <Tarunikaa_Radhakrishnan@comcast.com>

--- tests/test_l1_utils.h
+++ tests/test_l1_utils.h
@@ -2,7 +2,10 @@
 #include <numeric>
 #include <iomanip>
 #include <ostream>
+#include "al_service_registration_enums.h"
 
+using MacAddress = std::array<uint8_t, 6>;
+using MessageIdRange = std::pair<uint16_t, uint16_t>;
 
 MacAddress parseMacAddress(const std::string& macStr);
 std::ostream& operator<<(std::ostream& os, const MacAddress& mac);
