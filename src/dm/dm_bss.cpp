9066ca6ac3f74ba96b6b5ee5decaffd3a9807c82
Nikita Hakai <nikita_hakai@comcast.com>
2025-09-03 10:01:13 -0700

    Configuring default VLAN ID based on haultypes for CLI

--- src/dm/dm_bss.cpp
+++ src/dm/dm_bss.cpp
@@ -178,6 +178,7 @@ void dm_bss_t::encode(cJSON *obj, bool summary)
     mac_addr_str_t  mac_str;
     unsigned short i;
 	em_short_string_t	haul_type_str;
+    unsigned int vlan = 0;
 
     dm_easy_mesh_t::macbytes_to_string(m_bss_info.bssid.mac, mac_str);
     cJSON_AddStringToObject(obj, "BSSID", mac_str);
@@ -185,22 +186,27 @@ void dm_bss_t::encode(cJSON *obj, bool summary)
 	switch (m_bss_info.id.haul_type) {
 		case em_haul_type_fronthaul:
 			strncpy(haul_type_str, "Fronthaul", strlen("Fronthaul") + 1);
+			vlan = 12;
 			break;
 
 		case em_haul_type_backhaul:
 			strncpy(haul_type_str, "Backhaul", strlen("Backhaul") + 1);
+			vlan = 13;
 			break;
 
 		case em_haul_type_iot:
 			strncpy(haul_type_str, "IoT", strlen("IoT") + 1);
+			vlan = 14;
 			break;
 
 		case em_haul_type_configurator:
 			strncpy(haul_type_str, "Configurator", strlen("Configurator") + 1);
+			vlan = 15;
 			break;
 
 		case em_haul_type_hotspot:
 			strncpy(haul_type_str, "Hotspot", strlen("Hotspot") + 1);
+			vlan = 16;
 			break;
 
 		default:
@@ -212,6 +218,7 @@ void dm_bss_t::encode(cJSON *obj, bool summary)
     cJSON_AddBoolToObject(obj, "Enabled", m_bss_info.enabled);
     cJSON_AddStringToObject(obj, "TimeStamp", m_bss_info.timestamp);
     cJSON_AddNumberToObject(obj, "VapMode", m_bss_info.vap_mode);
+    cJSON_AddNumberToObject(obj, "VlanID", vlan);
     
 	if (summary == true) {
         return;
@@ -301,6 +308,7 @@ void dm_bss_t::operator = (const dm_bss_t& obj)
     memcpy(this->m_bss_info.vendor_elements, obj.m_bss_info.vendor_elements, sizeof(this->m_bss_info.vendor_elements));
     this->m_bss_info.vendor_elements_len = obj.m_bss_info.vendor_elements_len;
     this->m_bss_info.connect_status = obj.m_bss_info.connect_status;
+    this->m_bss_info.vlan_id = obj.m_bss_info.vlan_id;
 }
 
 
@@ -342,6 +350,7 @@ bool dm_bss_t::operator == (const dm_bss_t& obj)
     ret += (memcmp(this->m_bss_info.vendor_elements, obj.m_bss_info.vendor_elements, sizeof(this->m_bss_info.vendor_elements)) != 0);
     ret += !(this->m_bss_info.vendor_elements_len == obj.m_bss_info.vendor_elements_len);
     ret += !(this->m_bss_info.connect_status == obj.m_bss_info.connect_status);
+    ret += !(this->m_bss_info.vlan_id == obj.m_bss_info.vlan_id);
 
     if (ret > 0)
         return false;
