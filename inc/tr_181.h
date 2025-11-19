9df9e84e5b72ba4c99499579d9eb2327ba17ce1e
sriraaman-c <sriraaman33@gmail.com>
2025-11-19 18:29:55 +0000

    Registration and DeviceList dynamic row handling fix
    Signed-off-by: sriraaman-c <sriraaman33@gmail.com>

--- inc/tr_181.h
+++ inc/tr_181.h
@@ -261,8 +261,8 @@ class dm_easy_mesh_ctrl_t;
 
 class tr_181_t {
 private:
-    static bus_handle_t m_bus_handle;//remove static
-    static dm_easy_mesh_ctrl_t* m_ctrl;// remove static
+    bus_handle_t m_bus_handle;
+    dm_easy_mesh_ctrl_t* m_ctrl;
 
 public:
 
@@ -297,7 +297,7 @@ public:
     static bus_error_t default_event_sub_handler(char* eventName, bus_event_sub_action_t action, int32_t interval, bool* autoPublish);
 
     //Table callback handlers
-    static bus_error_t add_table_row(char const *name);
+    static bus_error_t add_table_row(char const *name, dm_easy_mesh_t *dm);
 
     //Network
     static bus_error_t network_get(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data);
