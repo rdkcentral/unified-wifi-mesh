4e5bb7f426dad2b5ed4856f1f70da65e6ca4ffb6
Nikita Hakai <nikita_hakai@comcast.com>
2026-07-30 13:11:13 -0700

    wei changes compile error fix

--- inc/em_base.h
+++ inc/em_base.h
@@ -3719,7 +3719,6 @@ static const SecurityTypeMap securityTypeMap[] = {
 
 static const unsigned char comcast_vendor_oui[EM_VENDOR_OUI_SIZE] = {0xd8, 0x9c, 0x8e};
 static const unsigned char airties_vendor_oui[EM_VENDOR_OUI_SIZE] = {0x88, 0x41, 0xfc};
-static const unsigned char comcast_vendor_oui[EM_VENDOR_OUI_SIZE] = {0xd8, 0x9c, 0x8e};
 
 #ifndef SSL_KEY
 #if OPENSSL_VERSION_NUMBER < 0x30000000L
