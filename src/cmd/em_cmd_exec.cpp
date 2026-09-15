0cd0b1dbf5b39bc14a025eab58d22a98db79064a
Yashodha KB <yakshagowda37@gmail.com>
2026-09-15 14:22:21 +0530

    RDKBDEV-3508: Fix socket resource leak

--- src/cmd/em_cmd_exec.cpp
+++ src/cmd/em_cmd_exec.cpp
@@ -329,7 +329,7 @@ int em_cmd_exec_t::send_cmd(em_service_type_t to_svc, unsigned char *in, unsigne
     if ((ret = connect(dsock, reinterpret_cast<const struct sockaddr *> (&addr), sizeof(struct sockaddr_un))) != 0) {
         snprintf(out, out_len, "%s:%d: connect error on socket, err:%d\n", __func__, __LINE__, errno);
         close(dsock);
-    return -1;
+        return -1;
     }
 
     ssl = SSL_new(m_ssl_ctx);
